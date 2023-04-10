278f6f06f7b53228895e3df69404af686ee2e86c
Kozin Valentin <kozinvo333@gmail.com>
2023-04-10 20:12:31 +0500

    lesson 6

---
+++ index.ts
@@ -0,0 +1,15 @@
+// let userName: string = 'Ivan';
+
+// userName = 5;
+
+// userName.isInteger()
+
+const isBirthday: boolean = true;
+let age: number;
+const userName: string = 'Ivan';
+
+age = 40;
+
+if (isBirthday) {
+  console.log(`Congrats ${userName.toUpperCase()}, age: ${age + 1}`);
+}
